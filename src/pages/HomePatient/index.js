e0068a8c0be7872cf89abb5f023ab8125e09fbf2
rainanaraujo <rainanaraaujo@gmail.com>
2021-07-09 16:18:59 -0300

    Added component menu and animations

--- src/pages/HomePatient/index.js
+++ src/pages/HomePatient/index.js
@@ -21,48 +21,54 @@ import { Feather } from "@expo/vector-icons";
 export default function HomePatient() {
   const [expandedMenu, setExpandedMenu] = useState(false);
   return (
-    <Container>
-      <StatusBar backgroundColor="#fff" />
-      <Menu open={expandedMenu} onClose={() => setExpandedMenu(false)} />
-      <Header>
-        <ButtonMenu onPress={() => setExpandedMenu(true)}>
-          <Feather name="menu" size={24} color="#070C17" />
-        </ButtonMenu>
-        <Profile>
-          <Name>Jasmim Pereira</Name>
-          <Avatar
-            source={{
-              uri: "https://psicoter.com.br/wp-content/uploads/2019/01/pessoa-flexivel-seja-mais-flexivel-800x533.jpg",
-            }}
-          />
-        </Profile>
-      </Header>
-      <Title>Dashboard</Title>
-      <Topic>Avisos</Topic>
-      <ButtonNotification
-        title="Nova agenda de atendimento"
-        Icon={() => (
-          <MaterialCommunityIcons
-            name="account-clock-outline"
-            size={20}
-            color="#fff"
-          />
-        )}
-      />
-      <Topic>Sobre você</Topic>
-      <CardButton
-        category="Alerta"
-        description="Registrar alguma grande experiência positiva ou negativa neste momento."
-        title="Está acontecendo algo?"
-        Icon={() => <FontAwesome5 name="heartbeat" size={18} color="#FFD4D4" />}
-        type="red"
-      />
-      <CardButton
-        category="Ultima Semana"
-        description="Baseado em seus relatos diários, tristeza foi a tag utilizada com mais frequência."
-        title="Tristeza"
-        Icon={() => <MaterialIcons name="history" size={18} color="#929292" />}
-      />
-    </Container>
+    <SafeAreaView style={{ backgroundColor: "#fff" }}>
+      <Container>
+        <StatusBar backgroundColor="#fff" />
+        <Menu open={expandedMenu} onClose={() => setExpandedMenu(false)} />
+        <Header>
+          <ButtonMenu onPress={() => setExpandedMenu(true)}>
+            <Feather name="menu" size={24} color="#070C17" />
+          </ButtonMenu>
+          <Profile>
+            <Name>Jasmim Pereira</Name>
+            <Avatar
+              source={{
+                uri: "https://psicoter.com.br/wp-content/uploads/2019/01/pessoa-flexivel-seja-mais-flexivel-800x533.jpg",
+              }}
+            />
+          </Profile>
+        </Header>
+        <Title>Dashboard</Title>
+        <Topic>Avisos</Topic>
+        <ButtonNotification
+          title="Nova agenda de atendimento"
+          Icon={() => (
+            <MaterialCommunityIcons
+              name="account-clock-outline"
+              size={20}
+              color="#fff"
+            />
+          )}
+        />
+        <Topic>Sobre você</Topic>
+        <CardButton
+          category="Alerta"
+          description="Registrar alguma grande experiência positiva ou negativa neste momento."
+          title="Está acontecendo algo?"
+          Icon={() => (
+            <FontAwesome5 name="heartbeat" size={18} color="#FFD4D4" />
+          )}
+          type="red"
+        />
+        <CardButton
+          category="Ultima Semana"
+          description="Baseado em seus relatos diários, tristeza foi a tag utilizada com mais frequência."
+          title="Tristeza"
+          Icon={() => (
+            <MaterialIcons name="history" size={18} color="#929292" />
+          )}
+        />
+      </Container>
+    </SafeAreaView>
   );
 }
